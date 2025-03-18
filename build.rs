65dba012d7dd63ef74626353e8bbe12237d5107c
arabian <a.tevg@ya.ru>
2025-03-18 20:40:05 +0300

    fixed building on linux

--- build.rs
+++ build.rs
@@ -5,10 +5,22 @@ use std::path::Path;
 fn main() {
     let switch_tools_dir = Path::new("switch-tools");
 
-    let autogen_status = Command::new("./autogen.sh")
+    let aclocal_status = Command::new("aclocal")
         .current_dir(switch_tools_dir)
-        .status()
-        .expect("Couldn't run autogen.sh");
+        .status().
+        expect("Couldn't run aclocal");
+
+    let autoconf_status = Command::new("autoconf")
+        .current_dir(switch_tools_dir)
+        .status().
+        expect("Couldn't run autoconf");
+
+    let automake_status = Command::new("automake")
+        .current_dir(switch_tools_dir)
+        .arg("--add-missing")
+        .arg("-c")
+        .status().
+        expect("Couldn't run automake");
 
     let conf_status = Command::new("./configure")
         .current_dir(switch_tools_dir)
@@ -20,7 +32,9 @@ fn main() {
         .status()
         .expect("Couldn't run make");
 
-    if !autogen_status.success() || !conf_status.success() || !make_status.success() {
-        panic!("Something went wrong trying to build switch-tools");
-    }
+    assert!(aclocal_status.success(), "Something went wrong while building switch-tools");
+    assert!(autoconf_status.success(), "Something went wrong while building switch-tools");
+    assert!(automake_status.success(), "Something went wrong while building switch-tools");
+    assert!(conf_status.success(), "Something went wrong while building switch-tools");
+    assert!(make_status.success(), "Something went wrong while building switch-tools");
 }
